b4355b86fae7fe3b67753cd4f7ed5945c796814f
Alexey Naumov <alexey.naumov@chimpchange.me>
2019-11-16 02:39:54 +0300

    Fix min required os versions

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ViewInspector",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v9), .watchOS(.v3)
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         .library(
