a09c0e3b3ab3b81053502d04223de1bf8f5dd9f7
Alexey Naumov <alexey.naumov@chimpchange.me>
2019-11-17 01:27:36 +0300

    Add more tests for any view

--- Tests/ViewInspectorTests/AnyViewTests.swift
+++ Tests/ViewInspectorTests/AnyViewTests.swift
@@ -11,7 +11,23 @@ final class AnyViewTests: XCTestCase {
         XCTAssertEqual(sut, sampleView)
     }
     
+    func testExtractionFromSingleViewContainer() throws {
+        let view = Button(action: { }, label: { AnyView(Text("")) })
+        XCTAssertNoThrow(try view.inspect().anyView())
+    }
+    
+    func testExtractionFromMultipleViewContainer() throws {
+        let view = HStack {
+            AnyView(Text(""))
+            AnyView(Text(""))
+        }
+        XCTAssertNoThrow(try view.inspect().anyView(0))
+        XCTAssertNoThrow(try view.inspect().anyView(1))
+    }
+    
     static var allTests = [
         ("testEnclosedView", testEnclosedView),
+        ("testExtractionFromSingleViewContainer", testExtractionFromSingleViewContainer),
+        ("testExtractionFromMultipleViewContainer", testExtractionFromMultipleViewContainer),
     ]
 }
