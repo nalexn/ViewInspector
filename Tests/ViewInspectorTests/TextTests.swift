79a4885716a40ed9f856c957b1971d15a186f185
Alexey Naumov <alexey.naumov@chimpchange.me>
2019-11-16 06:40:41 +0300

    Add tests for Text

--- Tests/ViewInspectorTests/TextTests.swift
+++ Tests/ViewInspectorTests/TextTests.swift
@@ -4,13 +4,37 @@ import SwiftUI
 
 final class TextTests: XCTestCase {
     
+    // MARK: - string()
+    
     func testLocalizableStringNoParams() throws {
         let view = Text("Test")
         let sut = try view.inspect().string()
         XCTAssertEqual(sut, "Test")
     }
-
+    
+    func testLocalizableStringWithOneParam() throws {
+        let view = Text("Test \(12)")
+        let sut = try view.inspect().string()
+        XCTAssertEqual(sut, "Test 12")
+    }
+    
+    func testLocalizableStringWithMultipleParams() throws {
+        let view = Text("Test \(12) \(5.7) \("abc")")
+        let sut = try view.inspect().string()
+        XCTAssertEqual(sut, "Test 12 5.7 abc")
+    }
+    
+    func testExternalStringValue() throws {
+        let string = "Test"
+        let view = Text(string)
+        let sut = try view.inspect().string()
+        XCTAssertEqual(sut, string)
+    }
+    
     static var allTests = [
         ("testLocalizableStringNoParams", testLocalizableStringNoParams),
+        ("testLocalizableStringWithOneParam", testLocalizableStringWithOneParam),
+        ("testLocalizableStringWithMultipleParams", testLocalizableStringWithMultipleParams),
+        ("testExternalStringValue", testExternalStringValue),
     ]
 }
