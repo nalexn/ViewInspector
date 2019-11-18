dbfd621c6a32429e92e8e90ab08a5c899c9273d2
Alexey Naumov <alexey.naumov@chimpchange.me>
2019-11-18 23:21:33 +0300

    Add ModifiedContent and tests

---
+++ Sources/ViewInspector/SwiftUI/ModifiedContent.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+#if !os(watchOS)
+
+public extension ViewType {
+    
+    struct ModifiedContent: KnownViewType {
+        public static var typePrefix: String = "ModifiedContent"
+    }
+}
+
+public extension ModifiedContent {
+    
+    func inspect() throws -> InspectableView<ViewType.ModifiedContent> {
+        return try InspectableView<ViewType.ModifiedContent>(self)
+    }
+}
+
+// MARK: - SingleViewContent
+
+extension ViewType.ModifiedContent: SingleViewContent {
+    
+    public static func content(view: Any) throws -> Any {
+        return try Inspector.attribute(path: "content", value: view)
+    }
+}
+
+// MARK: - SingleViewContent
+
+public extension InspectableView where View: SingleViewContent {
+    
+    func modifiedContent() throws -> InspectableView<ViewType.ModifiedContent> {
+        let content = try View.content(view: view)
+        return try InspectableView<ViewType.ModifiedContent>(content)
+    }
+}
+
+// MARK: - MultipleViewContent
+
+public extension InspectableView where View: MultipleViewContent {
+    
+    func modifiedContent(_ index: Int) throws -> InspectableView<ViewType.ModifiedContent> {
+        let content = try contentView(at: index)
+        return try InspectableView<ViewType.ModifiedContent>(content)
+    }
+}
+
+#endif
---
+++ Tests/ViewInspectorTests/SwiftUI/ModifiedContentTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+import SwiftUI
+@testable import ViewInspector
+
+#if !os(watchOS)
+
+final class ModifiedContentTests: XCTestCase {
+    
+    func testEnclosedView() throws {
+        let sampleView = Text("Test")
+        let view = ModifiedContent(content: sampleView, modifier: TestModifier())
+        let sut = try view.inspect().text().view as? Text
+        XCTAssertEqual(sut, sampleView)
+    }
+    
+    func testExtractionFromSingleViewContainer() throws {
+        let view = AnyView(ModifiedContent(content: Text("Test"),
+                                           modifier: TestModifier()))
+        XCTAssertNoThrow(try view.inspect().modifiedContent())
+    }
+    
+    func testExtractionFromMultipleViewContainer() throws {
+        let view = HStack {
+            ModifiedContent(content: Text("Test"), modifier: TestModifier())
+            ModifiedContent(content: Text("Test"), modifier: TestModifier())
+        }
+        XCTAssertNoThrow(try view.inspect().modifiedContent(0))
+        XCTAssertNoThrow(try view.inspect().modifiedContent(1))
+    }
+    
+    static var allTests = [
+        ("testEnclosedView", testEnclosedView),
+        ("testExtractionFromSingleViewContainer", testExtractionFromSingleViewContainer),
+        ("testExtractionFromMultipleViewContainer", testExtractionFromMultipleViewContainer),
+    ]
+}
+
+private struct TestModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+    }
+}
+
+#endif
