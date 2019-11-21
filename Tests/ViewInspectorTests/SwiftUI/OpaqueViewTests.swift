1a4c8d92e8e5ad26b3ae3e3f009eef1eaa8685e2
Alexey Naumov <alexey.naumov@chimpchange.me>
2019-11-21 19:46:13 +0300

    Add tests for opaque view

---
+++ Tests/ViewInspectorTests/SwiftUI/OpaqueViewTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+import SwiftUI
+@testable import ViewInspector
+
+final class OpaqueViewTests: XCTestCase {
+    
+    func testOpaqueAnyView() throws {
+        let view = Text("Test").padding()
+        let string = try view.inspect().text().string()
+        XCTAssertEqual(string, "Test")
+    }
+    
+    func testOpaqueInspectableView() throws {
+        let view = InspectableTestView().padding()
+        let string = try view.inspect(InspectableTestView.self).text().string()
+        XCTAssertEqual(string, "Test")
+    }
+    
+    func testOpaqueEnvInspectableView() throws {
+        let view = EnvInspectableTestView().padding()
+        let state = EnvInspectableTestView.State()
+        let string = try view.inspect(EnvInspectableTestView.self, state).text().string()
+        XCTAssertEqual(string, "Test")
+    }
+}
+
+private struct InspectableTestView: View, Inspectable {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+private struct EnvInspectableTestView: View, InspectableWithEnvObject {
+    
+    var body: Body {
+        content(State())
+    }
+    
+    func content(_ state: State) -> some View {
+        Text("Test")
+    }
+    
+    class State: ObservableObject { }
+}
