52e3fa769323496b88fe288588d5a212068b9ab5
Alexey Naumov <a.naumov91@gmail.com>
2019-11-29 18:38:57 +0300

    Add tests for mixed optional and non optional views

--- Tests/ViewInspectorTests/SwiftUI/OptionalViewTests.swift
+++ Tests/ViewInspectorTests/SwiftUI/OptionalViewTests.swift
@@ -5,23 +5,49 @@ import SwiftUI
 final class OptionalViewTests: XCTestCase {
     
     func testOptionalViewWhenExists() throws {
-        let view = TestView(flag: true)
+        let view = OptionalView(flag: true)
         let string = try view.inspect().hStack().text(0).string()
         XCTAssertEqual(string, "ABC")
     }
     
     func testOptionalViewWhenIsAbsent() throws {
-        let view = TestView(flag: false)
+        let view = OptionalView(flag: false)
         XCTAssertThrowsError(try view.inspect().hStack().text(0))
+    }
+    
+    func testMixedOptionalViewWhenExists() throws {
+        let view = MixedOptionalView(flag: true)
+        let string1 = try view.inspect().hStack().text(0).string()
+        XCTAssertEqual(string1, "ABC")
+        let string2 = try view.inspect().hStack().text(1).string()
+        XCTAssertEqual(string2, "XYZ")
+    }
+    
+    func testMixedOptionalViewWhenIsAbsent() throws {
+        let view = MixedOptionalView(flag: false)
+        XCTAssertThrowsError(try view.inspect().hStack().text(0))
+        let string = try view.inspect().hStack().text(1).string()
+        XCTAssertEqual(string, "XYZ")
     }
 }
 
-private struct TestView: View, Inspectable {
+private struct OptionalView: View, Inspectable {
     
     let flag: Bool
     var body: some View {
         HStack {
             if flag { Text("ABC") }
+        }
+    }
+}
+
+private struct MixedOptionalView: View, Inspectable {
+    
+    let flag: Bool
+    var body: some View {
+        HStack {
+            if flag { Text("ABC") }
+            Text("XYZ")
         }
     }
 }
