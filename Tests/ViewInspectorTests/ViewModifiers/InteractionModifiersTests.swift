fde919e46bd317469d54a24a01dd0704a4a591dc
Alexey Naumov <a.naumov91@gmail.com>
2019-12-13 20:46:46 +0300

    Add Accessibility Modifiers tests

--- Tests/ViewInspectorTests/ViewModifiers/InteractionModifiersTests.swift
+++ Tests/ViewInspectorTests/ViewModifiers/InteractionModifiersTests.swift
@@ -199,3 +199,78 @@ final class ViewDragDropTests: XCTestCase {
     }
     #endif
 }
+
+// MARK: - ViewAccessibilityTests
+
+final class ViewAccessibilityTests: XCTestCase {
+    
+    func testAccessibilityLabel() throws {
+        let sut = EmptyView().accessibility(label: Text(""))
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityValue() throws {
+        let sut = EmptyView().accessibility(value: Text(""))
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityHint() throws {
+        let sut = EmptyView().accessibility(hint: Text(""))
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityHidden() throws {
+        let sut = EmptyView().accessibility(hidden: true)
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityIdentifier() throws {
+        let sut = EmptyView().accessibility(identifier: "")
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilitySelectionIdentifier() throws {
+        let sut = EmptyView().accessibility(selectionIdentifier: "")
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityActivationPoint() throws {
+        let sut = EmptyView().accessibility(activationPoint: .center)
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityAction() throws {
+        let sut = EmptyView().accessibilityAction(.default) { }
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityAdjustableAction() throws {
+        let sut = EmptyView().accessibilityAdjustableAction { _ in }
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityScrollAction() throws {
+        let sut = EmptyView().accessibilityScrollAction { _ in }
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityElement() throws {
+        let sut = EmptyView().accessibilityElement(children: .contain)
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityAddTraits() throws {
+        let sut = EmptyView().accessibility(addTraits: AccessibilityTraits())
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityRemoveTraits() throws {
+        let sut = EmptyView().accessibility(removeTraits: AccessibilityTraits())
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilitySortPriority() throws {
+        let sut = EmptyView().accessibility(sortPriority: 5)
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+}
