746dcaa0b1e619e6c02d1e3b40e8ee9742aaa26a
Alexey Naumov <a.naumov91@gmail.com>
2019-12-16 17:42:11 +0300

    Add accessibility modifiers

---
+++ Sources/ViewInspector/Modifiers/AccessibilityModifiers.swift
@@ -0,0 +1,121 @@
+import SwiftUI
+
+// MARK: - Accessibility
+
+public extension InspectableView {
+    
+    func accessibilityLabel() throws -> InspectableView<ViewType.Text> {
+        let text = try accessibilityElement(
+            "LabelKey", type: Text.self, call: "accessibilityLabel")
+        return try .init(Content(text))
+    }
+    
+    func accessibilityValue() throws -> InspectableView<ViewType.Text> {
+        let text = try accessibilityElement(
+            "TypedValueKey", path: "value|some|description|some",
+            type: Text.self, call: "accessibilityValue")
+        return try .init(Content(text))
+    }
+    
+    func accessibilityHint() throws -> InspectableView<ViewType.Text> {
+        let text = try accessibilityElement(
+            "HintKey", type: Text.self, call: "accessibilityHint")
+        return try .init(Content(text))
+    }
+    
+    func accessibilityHidden() throws -> Bool {
+        let visibility = try accessibilityElement(
+            "VisibilityKey", path: "value", type: (Any?).self, call: "accessibility(hidden:)")
+        switch visibility {
+        case let .some(value):
+            return String(describing: value) == "hidden"
+        case .none:
+            return false
+        }
+    }
+    
+    func accessibilityIdentifier() throws -> String {
+        return try accessibilityElement(
+            "IdentifierKey", type: String.self, call: "accessibility(identifier:)")
+    }
+    
+    func accessibilitySelectionIdentifier() throws -> AnyHashable {
+        return try accessibilityElement(
+            "SelectionIdentifierKey", type: AnyHashable.self,
+            call: "accessibility(selectionIdentifier:)")
+    }
+    
+    func accessibilityActivationPoint() throws -> UnitPoint {
+        return try accessibilityElement(
+            "ActivationPointKey", path: "value|some|unitPoint",
+            type: UnitPoint.self, call: "accessibility(activationPoint:)")
+    }
+    
+    func callAccessibilityAction(_ kind: AccessibilityActionKind) throws {
+        let kindString = String(describing: kind)
+        let shortName = kindString
+            .components(separatedBy: CharacterSet(charactersIn: ".)"))
+            .filter { $0.count > 0 }.last!
+        let call = "callAccessibilityAction(.\(shortName))"
+        typealias Callback = (()) -> Void
+        let callback = try accessibilityAction(name: kindString, path: "box|action|kind",
+                                               type: Callback.self, call: call)
+        callback(())
+    }
+    
+    func callAccessibilityAdjustableAction(_ direction: AccessibilityAdjustmentDirection = .increment) throws {
+        typealias Callback = (AccessibilityAdjustmentDirection) -> Void
+        let callback = try accessibilityAction(
+            name: "AccessibilityAdjustableAction()", path: "box|action",
+            type: Callback.self, call: "accessibilityAdjustableAction")
+        callback(direction)
+    }
+    
+    func callAccessibilityScrollAction(_ edge: Edge) throws {
+        typealias Callback = (Edge) -> Void
+        let callback = try accessibilityAction(
+            name: "AccessibilityScrollAction()", path: "box|action",
+            type: Callback.self, call: "accessibilityScrollAction")
+        callback(edge)
+    }
+    
+    func accessibilitySortPriority() throws -> Double {
+        return try accessibilityElement(
+            "SortPriorityKey", type: Double.self, call: "accessibility(sortPriority:)")
+    }
+}
+
+// MARK: - Private
+
+private extension InspectableView {
+    
+    func accessibilityElement<T>(_ name: String, path: String = "value|some",
+                                 type: T.Type, call: String) throws -> T {
+        let element = try modifierAttribute(
+            modifierName: "AccessibilityAttachmentModifier",
+            path: "modifier|attachment|some|properties|plist|elements|some",
+            type: Any.self, call: call)
+        guard Inspector.typeName(value: element).contains(name),
+            let value = try? Inspector.attribute(path: path, value: element) as? T else {
+            throw InspectionError.modifierNotFound(parent:
+                Inspector.typeName(value: content.view), modifier: call)
+        }
+        return value
+    }
+    
+    func accessibilityAction<T>(name: String, path: String, type: T.Type, call: String) throws -> T {
+        let actionHandlers = try accessibilityElement(
+            "ActionsKey", path: "value",
+            type: [Any].self, call: call)
+        guard let handler = actionHandlers.first(where: { handler -> Bool in
+            guard let actionName = try? Inspector.attribute(path: path, value: handler)
+                else { return false }
+            return name == String(describing: actionName)
+        }), let callback = try? Inspector.attribute(path: "box|handler", value: handler) as? T
+        else {
+            throw InspectionError.modifierNotFound(parent:
+                Inspector.typeName(value: content.view), modifier: call)
+        }
+        return callback
+    }
+}
--- Tests/ViewInspectorTests/ViewModifiers/AccessibilityModifiersTests.swift
+++ Tests/ViewInspectorTests/ViewModifiers/AccessibilityModifiersTests.swift
@@ -12,9 +12,23 @@ final class ViewAccessibilityTests: XCTestCase {
         XCTAssertNoThrow(try sut.inspect().emptyView())
     }
     
+    func testAccessibilityLabelInspection() throws {
+        let string = "abc"
+        let sut = try EmptyView().accessibility(label: Text(string))
+            .inspect().emptyView().accessibilityLabel().string()
+        XCTAssertEqual(sut, string)
+    }
+    
     func testAccessibilityValue() throws {
         let sut = EmptyView().accessibility(value: Text(""))
         XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityValueInspection() throws {
+        let string = "abc"
+        let sut = try EmptyView().accessibility(value: Text(string))
+            .inspect().emptyView().accessibilityValue().string()
+        XCTAssertEqual(sut, string)
     }
     
     func testAccessibilityHint() throws {
@@ -22,9 +36,25 @@ final class ViewAccessibilityTests: XCTestCase {
         XCTAssertNoThrow(try sut.inspect().emptyView())
     }
     
+    func testAccessibilityHintInspection() throws {
+        let string = "abc"
+        let sut = try EmptyView().accessibility(hint: Text(string))
+            .inspect().emptyView().accessibilityHint().string()
+        XCTAssertEqual(sut, string)
+    }
+    
     func testAccessibilityHidden() throws {
-        let sut = EmptyView().accessibility(hidden: true)
+        let sut = EmptyView().accessibility(hidden: false)
         XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityHiddenInspection() throws {
+        let sut1 = try EmptyView().accessibility(hidden: true)
+            .inspect().emptyView().accessibilityHidden()
+        XCTAssertTrue(sut1)
+        let sut2 = try EmptyView().accessibility(hidden: false)
+            .inspect().emptyView().accessibilityHidden()
+        XCTAssertFalse(sut2)
     }
     
     func testAccessibilityIdentifier() throws {
@@ -32,9 +62,23 @@ final class ViewAccessibilityTests: XCTestCase {
         XCTAssertNoThrow(try sut.inspect().emptyView())
     }
     
+    func testAccessibilityIdentifierInspection() throws {
+        let string = "abc"
+        let sut = try EmptyView().accessibility(identifier: string)
+            .inspect().emptyView().accessibilityIdentifier()
+        XCTAssertEqual(sut, string)
+    }
+    
     func testAccessibilitySelectionIdentifier() throws {
         let sut = EmptyView().accessibility(selectionIdentifier: "")
         XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilitySelectionIdentifierInspection() throws {
+        let string = "abc"
+        let sut = try EmptyView().accessibility(selectionIdentifier: string)
+            .inspect().emptyView().accessibilitySelectionIdentifier()
+        XCTAssertEqual(sut, string)
     }
     
     func testAccessibilityActivationPoint() throws {
@@ -42,9 +86,49 @@ final class ViewAccessibilityTests: XCTestCase {
         XCTAssertNoThrow(try sut.inspect().emptyView())
     }
     
+    func testAccessibilityActivationPointInspection() throws {
+        let point: UnitPoint = .bottomTrailing
+        let sut = try EmptyView().accessibility(activationPoint: point)
+            .inspect().emptyView().accessibilityActivationPoint()
+        XCTAssertEqual(sut, point)
+    }
+    
     func testAccessibilityAction() throws {
         let sut = EmptyView().accessibilityAction(.default) { }
         XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityActionInspection() throws {
+        let exp = XCTestExpectation(description: "accessibilityAction")
+        let sut = EmptyView().accessibilityAction(.default) {
+            exp.fulfill()
+        }
+        try sut.inspect().emptyView().callAccessibilityAction(.default)
+        wait(for: [exp], timeout: 0.1)
+    }
+    
+    func testAccessibilityActionInspectionError() throws {
+        let sut = EmptyView().accessibilityAction(.escape) { }
+        XCTAssertThrowsError(try sut.inspect().emptyView().callAccessibilityAction(.default))
+    }
+    
+    func testAccessibilityActionInspectionMultipleCallbacks() throws {
+        let exp1 = XCTestExpectation(description: "accessibilityAction1")
+        let exp2 = XCTestExpectation(description: "accessibilityAction2")
+        exp1.assertForOverFulfill = true
+        exp2.assertForOverFulfill = true
+        exp1.expectedFulfillmentCount = 1
+        exp2.expectedFulfillmentCount = 2
+        let sut = EmptyView().accessibilityAction(.default) {
+            exp1.fulfill()
+        }.accessibilityAction(.escape) {
+            exp2.fulfill()
+        }
+        let view = try sut.inspect().emptyView()
+        try view.callAccessibilityAction(.escape)
+        try view.callAccessibilityAction(.default)
+        try view.callAccessibilityAction(.escape)
+        wait(for: [exp1, exp2], timeout: 0.1)
     }
     
     func testAccessibilityAdjustableAction() throws {
@@ -52,9 +136,29 @@ final class ViewAccessibilityTests: XCTestCase {
         XCTAssertNoThrow(try sut.inspect().emptyView())
     }
     
+    func testAccessibilityAdjustableActionInspection() throws {
+        let exp = XCTestExpectation(description: "accessibilityAdjustableAction")
+        let sut = EmptyView().accessibilityAdjustableAction { direction in
+            XCTAssertEqual(direction, .decrement)
+            exp.fulfill()
+        }
+        try sut.inspect().emptyView().callAccessibilityAdjustableAction(.decrement)
+        wait(for: [exp], timeout: 0.1)
+    }
+    
     func testAccessibilityScrollAction() throws {
         let sut = EmptyView().accessibilityScrollAction { _ in }
         XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilityScrollActionInspection() throws {
+        let exp = XCTestExpectation(description: "accessibilityScrollAction")
+        let sut = EmptyView().accessibilityScrollAction { edge in
+            XCTAssertEqual(edge, .leading)
+            exp.fulfill()
+        }
+        try sut.inspect().emptyView().callAccessibilityScrollAction(.leading)
+        wait(for: [exp], timeout: 0.1)
     }
     
     func testAccessibilityElement() throws {
@@ -75,5 +179,11 @@ final class ViewAccessibilityTests: XCTestCase {
     func testAccessibilitySortPriority() throws {
         let sut = EmptyView().accessibility(sortPriority: 5)
         XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testAccessibilitySortPriorityInspection() throws {
+        let sut = try EmptyView().accessibility(sortPriority: 6)
+            .inspect().emptyView().accessibilitySortPriority()
+        XCTAssertEqual(sut, 6)
     }
 }
