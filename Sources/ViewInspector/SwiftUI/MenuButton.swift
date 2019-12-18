7e4631146747221c2d9dcd82f780808e57bf7f37
Alexey Naumov <a.naumov91@gmail.com>
2019-12-18 15:30:22 +0300

    Add MenuButton and tests

---
+++ Sources/ViewInspector/SwiftUI/MenuButton.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+
+public extension ViewType {
+    
+    struct MenuButton: KnownViewType {
+        public static var typePrefix: String = "MenuButton"
+    }
+}
+
+public extension MenuButton {
+    
+    func inspect() throws -> InspectableView<ViewType.MenuButton> {
+        return try .init(ViewInspector.Content(self))
+    }
+}
+
+public extension ViewType.MenuButton {
+    
+    struct Label: KnownViewType {
+        public static var typePrefix: String = "MenuButton"
+    }
+}
+
+// MARK: - Content Extraction
+
+extension ViewType.MenuButton: SingleViewContent {
+    
+    public static func child(_ content: Content, envObject: Any) throws -> Content {
+        let view = try Inspector.attribute(label: "content", value: content.view)
+        return try Inspector.unwrap(view: view, modifiers: [])
+    }
+}
+
+// MARK: - Extraction from SingleViewContent parent
+
+public extension InspectableView where View: SingleViewContent {
+    
+    func menuButton() throws -> InspectableView<ViewType.MenuButton> {
+        return try .init(try child())
+    }
+}
+
+extension ViewType.MenuButton.Label: SingleViewContent {
+    
+    public static func child(_ content: Content, envObject: Any) throws -> Content {
+        let view = try Inspector.attribute(label: "label", value: content.view)
+        return try Inspector.unwrap(view: view, modifiers: [])
+    }
+}
+
+// MARK: - Extraction from MultipleViewContent parent
+
+public extension InspectableView where View: MultipleViewContent {
+    
+    func menuButton(_ index: Int) throws -> InspectableView<ViewType.MenuButton> {
+        return try .init(try child(at: index))
+    }
+}
+
+// MARK: - Custom Attributes
+
+public extension InspectableView where View == ViewType.MenuButton {
+    
+    func label() throws -> InspectableView<ViewType.MenuButton.Label> {
+        return try .init(content)
+    }
+}
--- Tests/ViewInspectorTests/SwiftUI/MenuButtonTests.swift
+++ Tests/ViewInspectorTests/SwiftUI/MenuButtonTests.swift
@@ -2,9 +2,43 @@ import XCTest
 import SwiftUI
 @testable import ViewInspector
 
+final class MenuButtonTests: XCTestCase {
+    
+    func testEnclosedView() throws {
+        let sut = MenuButton(label: Text("")) { EmptyView() }
+        XCTAssertNoThrow(try sut.inspect().emptyView())
+    }
+    
+    func testResetsModifiers() throws {
+        let view = MenuButton(label: Text("")) { EmptyView() }.padding()
+        let sut = try view.inspect().menuButton().emptyView()
+        XCTAssertEqual(sut.content.modifiers.count, 0)
+    }
+    
+    func testExtractionFromSingleViewContainer() throws {
+        let view = AnyView(MenuButton(label: Text("")) { EmptyView() })
+        XCTAssertNoThrow(try view.inspect().menuButton())
+    }
+    
+    func testExtractionFromMultipleViewContainer() throws {
+        let view = HStack {
+            MenuButton(label: Text("")) { EmptyView() }
+            MenuButton(label: Text("")) { EmptyView() }
+        }
+        XCTAssertNoThrow(try view.inspect().menuButton(0))
+        XCTAssertNoThrow(try view.inspect().menuButton(1))
+    }
+    
+    func testLabelView() throws {
+        let sut = MenuButton(label: Text("abc")) { EmptyView() }
+        let text = try sut.inspect().label().text().string()
+        XCTAssertEqual(text, "abc")
+    }
+}
+
 // MARK: - View Modifiers
 
-final class GlobalModifiersForMenuButton: XCTestCase {
+final class GlobalModifiersForMenuMenuButton: XCTestCase {
     
     #if os(macOS)
     func testMenuButtonStyle() throws {
