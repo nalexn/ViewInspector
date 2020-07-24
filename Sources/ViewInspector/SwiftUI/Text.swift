d1b6e700d75f7abfdb6fa295f8e49850ea7cddf0
Vinícius Uzêda <vinicius.uzeda@symphony.com>
2020-07-24 17:22:13 -0300

    Add a `attributedString` inspector to check for `Text`'s style more easily

--- Sources/ViewInspector/SwiftUI/Text.swift
+++ Sources/ViewInspector/SwiftUI/Text.swift
@@ -65,4 +65,48 @@ public extension InspectableView where View == ViewType.Text {
         }
         return String(format: format, arguments: values)
     }
+
+    func attributedString() throws -> NSAttributedString? {
+        if let first = try? Inspector.attribute(path: "storage|anyTextStorage|first", value: content.view) as? Text,
+            let second = try? Inspector.attribute(path: "storage|anyTextStorage|second", value: content.view) as? Text {
+            let combination = NSMutableAttributedString()
+            combination.append(try first.inspect().text().attributedString() ?? NSAttributedString(string: ""))
+            combination.append(try second.inspect().text().attributedString() ?? NSAttributedString(string: ""))
+            return combination
+        }
+        if let string = try string() {
+            let attributedString = NSMutableAttributedString(string: string)
+            let range = NSRange(location: 0, length: attributedString.length)
+
+            let viewModifiers = (try? Inspector.attribute(path: "modifiers", value: content.view) as? [Any]) ?? []
+            for viewModifier in viewModifiers {
+                if String(describing: viewModifier) == "anyTextModifier(SwiftUI.BoldTextModifier)" {
+                    attributedString.addAttributes([
+                        NSAttributedString.Key("Bold"): true,
+                    ], range: range)
+                } else if String(describing: viewModifier) == "italic" {
+                    attributedString.addAttributes([
+                        NSAttributedString.Key("Italic"): true,
+                    ], range: range)
+                } else if let fontProvider = try? Inspector.attribute(path: "font|some|provider|base", value: viewModifier) {
+                    if String(describing: type(of: fontProvider)) == "SystemProvider" {
+                        let size = try Inspector.attribute(path: "size", value: fontProvider) as! CGFloat
+                        let weight = try Inspector.attribute(path: "weight", value: fontProvider) as! Font.Weight
+                        let design = try Inspector.attribute(path: "design", value: fontProvider) as! Font.Design
+                        attributedString.addAttributes([
+                            NSAttributedString.Key("Font"): Font.system(size: size, weight: weight, design: design),
+                        ], range: range)
+                    }
+                } else if let fontWeight = try? Inspector.attribute(path: "weight|some", value: viewModifier) as? Font.Weight {
+                    attributedString.addAttributes([
+                        NSAttributedString.Key("FontWeight"): fontWeight,
+                    ], range: range)
+                } else {
+                    throw InspectionError.notSupported(String(describing: viewModifier))
+                }
+            }
+            return attributedString
+        }
+        return nil
+    }
 }
