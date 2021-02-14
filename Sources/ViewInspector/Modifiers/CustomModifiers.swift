ac2f781a48bad32f195a308654f0ef51eecfefb1
Gili Labs <gili.patrick.r@gili-labs.com>
2021-02-14 17:33:02 -0500

    Added customModifier<T>(keyPath:)

---
+++ Sources/ViewInspector/Modifiers/CustomModifiers.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+// MARK: - Custom Modifiers
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+public extension InspectableView {
+
+    func customModifier<T>(keyPath: WritableKeyPath<EnvironmentValues, T>) throws -> T {
+        let environmentValues = EnvironmentValues()
+        let defaultValue = environmentValues[keyPath: keyPath]
+        let reference = EmptyView().environment(keyPath, defaultValue)
+        let keyPath = try Inspector.environmentKeyPath(T.self, reference)
+        return try environmentModifier(keyPath: keyPath, call: "resizableFrameMinWidth")
+    }
+}
