eaba71c6d586be20c5cc6167406397d4e22779ab
Gili Labs <gili.patrick.r@gili-labs.com>
2021-04-26 13:53:34 -0400

    Defined an InspectorEmissary for ViewModifier.

--- Sources/ViewInspector/InspectionEmissary.swift
+++ Sources/ViewInspector/InspectionEmissary.swift
@@ -24,10 +24,83 @@ public protocol InspectionEmissary: class {
     ) -> XCTestExpectation where P: Publisher, P.Failure == Never
 }
 
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+public protocol InspectionEmissaryForViewModifier: class {
+    
+    associatedtype V: ViewModifier & Inspectable
+    typealias Inspection = (InspectableView<ViewType.ClassifiedView>) throws -> Void
+    
+    var notice: PassthroughSubject<UInt, Never> { get }
+    var callbacks: [UInt: (V.Body) -> Void] { get set }
+    
+    @discardableResult
+    func inspect(after delay: TimeInterval,
+                 function: String, file: StaticString, line: UInt,
+                 _ inspection: @escaping Inspection
+    ) -> XCTestExpectation
+    
+    @discardableResult
+    func inspect<P>(onReceive publisher: P,
+                    function: String, file: StaticString, line: UInt,
+                    _ inspection: @escaping Inspection
+    ) -> XCTestExpectation where P: Publisher, P.Failure == Never
+}
+
 // MARK: - Default Implementation
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
 public extension InspectionEmissary {
+    
+    @discardableResult
+    func inspect(after delay: TimeInterval = 0,
+                 function: String = #function, file: StaticString = #file, line: UInt = #line,
+                 _ inspection: @escaping Inspection
+    ) -> XCTestExpectation {
+        let exp = XCTestExpectation(description: "Inspection at line \(line)")
+        setup(inspection: inspection, expectation: exp, function: function, file: file, line: line)
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak notice] in
+            notice?.send(line)
+        }
+        return exp
+    }
+    
+    @discardableResult
+    func inspect<P>(onReceive publisher: P,
+                    function: String = #function, file: StaticString = #file, line: UInt = #line,
+                    _ inspection: @escaping Inspection
+    ) -> XCTestExpectation where P: Publisher, P.Failure == Never {
+        let exp = XCTestExpectation(description: "Inspection at line \(line)")
+        setup(inspection: inspection, expectation: exp, function: function, file: file, line: line)
+        var subscription: AnyCancellable?
+        _ = subscription
+        subscription = publisher.sink { [weak notice] _ in
+            subscription = nil
+            DispatchQueue.main.async {
+                notice?.send(line)
+            }
+        }
+        return exp
+    }
+    
+    private func setup(inspection: @escaping Inspection,
+                       expectation: XCTestExpectation,
+                       function: String, file: StaticString, line: UInt) {
+        callbacks[line] = { [weak self] view in
+            do {
+                try inspection(try view.inspect(function: function))
+            } catch let error {
+                XCTFail("\(error.localizedDescription)", file: file, line: line)
+            }
+            if self?.callbacks.count == 0 {
+                ViewHosting.expel(function: function)
+            }
+            expectation.fulfill()
+        }
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+public extension InspectionEmissaryForViewModifier {
     
     @discardableResult
     func inspect(after delay: TimeInterval = 0,
