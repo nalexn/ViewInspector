6ef6baa535ba484cf4b4fb5b7af5541d63c34db7
Alexey Naumov <alexey@naumov.tech>
2021-12-26 13:23:09 +0300

    Add support for SignInWithAppleButton

---
+++ Sources/ViewInspector/SwiftUI/SignInWithAppleButton.swift
@@ -0,0 +1,89 @@
+import SwiftUI
+#if canImport(AuthenticationServices)
+import AuthenticationServices
+#endif
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+public extension ViewType {
+    
+    struct SignInWithAppleButton: KnownViewType {
+        public static let typePrefix: String = "SignInWithAppleButton"
+        public static var namespacedPrefixes: [String] {
+            return ["_AuthenticationServices_SwiftUI." + typePrefix]
+        }
+        public static func inspectionCall(typeName: String) -> String {
+            return "signInWithAppleButton(\(ViewType.indexPlaceholder))"
+        }
+    }
+}
+
+// MARK: - Extraction from SingleViewContent parent
+
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+public extension InspectableView where View: SingleViewContent {
+    
+    func signInWithAppleButton() throws -> InspectableView<ViewType.SignInWithAppleButton> {
+        return try .init(try child(), parent: self)
+    }
+}
+
+// MARK: - Extraction from MultipleViewContent parent
+
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+public extension InspectableView where View: MultipleViewContent {
+    
+    func signInWithAppleButton(_ index: Int) throws -> InspectableView<ViewType.SignInWithAppleButton> {
+        return try .init(try child(at: index), parent: self, index: index)
+    }
+}
+
+// MARK: - Custom Attributes
+
+#if canImport(AuthenticationServices)
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+public extension InspectableView where View == ViewType.SignInWithAppleButton {
+    
+    func labelType() throws -> SignInWithAppleButton.Label {
+        let type = try Inspector.attribute(
+            path: "configuration|type", value: content.view,
+            type: ASAuthorizationAppleIDButton.ButtonType.self)
+        return SignInWithAppleButton.Label(type: type)
+    }
+    
+    @discardableResult
+    internal // no official support, see the comment in the test
+    func callOnRequest() throws -> ASAuthorizationAppleIDRequest {
+        typealias Closure = (ASAuthorizationAppleIDRequest) -> Void
+        let closure = try Inspector.attribute(
+            path: "configuration|onRequest", value: content.view, type: Closure.self)
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        closure(request)
+        return request
+    }
+    
+    func callOnCompletion(_ result: Result<ASAuthorization, Error>) throws {
+        typealias Closure = (Result<ASAuthorization, Error>) -> Void
+        let closure = try Inspector.attribute(
+            path: "configuration|onCompletion", value: content.view, type: Closure.self)
+        closure(result)
+    }
+}
+#endif
+
+#if canImport(AuthenticationServices)
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+extension SignInWithAppleButton.Label: BinaryEquatable {
+    init(type: ASAuthorizationAppleIDButton.ButtonType) {
+        switch type {
+        case .signIn:
+            self = .signIn
+        case .continue:
+            self = .continue
+        case .signUp:
+            self = .signUp
+        @unknown default:
+            self = .signIn
+        }
+    }
+}
+#endif
---
+++ Tests/ViewInspectorTests/SwiftUI/SignInWithAppleButtonTests.swift
@@ -0,0 +1,62 @@
+import XCTest
+import SwiftUI
+import AuthenticationServices.ASAuthorizationAppleIDButton
+@testable import ViewInspector
+
+@available(iOS 15.0, watchOS 8.0, *)
+@available(tvOS, unavailable)
+@available(macOS, unavailable)
+final class SignInWithAppleButtonTests: XCTestCase {
+    
+    private var newButton: SignInWithAppleButton {
+        SignInWithAppleButton(onRequest: { _ in }, onCompletion: { _ in })
+    }
+    
+    func testExtractionFromSingleViewContainer() throws {
+        let sut = AnyView(newButton)
+        XCTAssertNoThrow(try sut.inspect().anyView().signInWithAppleButton())
+    }
+    
+    func testExtractionFromMultipleViewContainer() throws {
+        let sut = HStack { newButton; newButton }
+        XCTAssertNoThrow(try sut.inspect().hStack().signInWithAppleButton(0))
+        XCTAssertNoThrow(try sut.inspect().hStack().signInWithAppleButton(1))
+    }
+    
+    func testSearch() throws {
+        let sut = AnyView(newButton)
+        XCTAssertEqual(try sut.inspect().find(ViewType.SignInWithAppleButton.self).pathToRoot,
+            "anyView().signInWithAppleButton()")
+    }
+    
+    func testLabelType() throws {
+        let sut1 = SignInWithAppleButton(.signIn, onRequest: { _ in }, onCompletion: { _ in })
+        let sut2 = SignInWithAppleButton(.signUp, onRequest: { _ in }, onCompletion: { _ in })
+        XCTAssertEqual(try sut1.inspect().signInWithAppleButton().labelType(), .signIn)
+        XCTAssertEqual(try sut2.inspect().signInWithAppleButton().labelType(), .signUp)
+    }
+    
+    func testCallOnRequest() throws {
+        let exp = XCTestExpectation(description: #function)
+        let sut = SignInWithAppleButton(onRequest: { request in
+            // Does not work yet: reference to object is corrupted inside closure
+            // Any request to the object causes a crash:
+            // request.requestedScopes = [.email, .fullName]
+            exp.fulfill()
+        }, onCompletion: { _ in })
+        let request = try sut.inspect().signInWithAppleButton().callOnRequest()
+//        XCTAssertEqual(request.requestedScopes, [.email, .fullName])
+        XCTAssertEqual(request.requestedScopes, nil)
+        wait(for: [exp], timeout: 0.1)
+    }
+    
+    func testCallOnCompletion() throws {
+        let exp = XCTestExpectation(description: #function)
+        let sut = SignInWithAppleButton(onRequest: { _ in }, onCompletion: { _ in
+            exp.fulfill()
+        })
+        try sut.inspect().signInWithAppleButton()
+            .callOnCompletion(.failure(InspectionError.notSupported("")))
+        wait(for: [exp], timeout: 0.1)
+    }
+}
