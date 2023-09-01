64e4af8953cbc77c333d1775011076518493c299
Alexey Naumov <alexey@naumov.tech>
2023-09-01 18:35:03 +0600

    #198: Add view hosting code

--- .watchOS/App/watchOSApp.swift
+++ .watchOS/App/watchOSApp.swift
@@ -1,10 +1,18 @@
 import SwiftUI
 
+final class ExtensionDelegate: NSObject, WKExtensionDelegate {
+    let testViewSubject = TestViewSubject([])
+}
+
 @main
 struct watchOS_Watch_AppApp: App {
+    
+    @WKExtensionDelegateAdaptor(ExtensionDelegate.self) var extDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .testable(extDelegate.testViewSubject)
         }
     }
 }
