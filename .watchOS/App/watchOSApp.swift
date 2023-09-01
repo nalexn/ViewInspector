c1c1901800a0eeba3be25433fc1997115cabc997
Alexey Naumov <alexey@naumov.tech>
2023-09-01 18:26:39 +0600

    #198: Replace watchOS project with a new one

---
+++ .watchOS/App/watchOSApp.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+@main
+struct watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("ViewInspector").padding()
+    }
+}
---
+++ .watchOS/AppTests/watchOS_Watch_AppTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import App
+
+final class watchOS_Watch_AppTests: XCTestCase {
+
+    func testExample() throws {
+    }
+}
