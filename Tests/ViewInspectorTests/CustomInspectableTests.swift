e77257540ec746ff696e82cc05967bab211f10fc
Bryan Keller <kellerbryan19@gmail.com>
2024-01-05 17:20:47 -0800

    Add tests

---
+++ Tests/ViewInspectorTests/CustomInspectableTests.swift
@@ -0,0 +1,35 @@
+import XCTest
+import SwiftUI
+@testable import ViewInspector
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+final class CustomInspectableTests: XCTestCase {
+
+    func testCustomInspectableViewRepresentable() throws {
+        let sut = CustomViewRepresentable(labels: ["1", "abc"])
+        XCTAssertNoThrow(try sut.inspect().find(text: "abc"))
+    }
+
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+private struct CustomViewRepresentable: UIViewRepresentable, CustomInspectable {
+
+    let labels: [String]
+
+    @ViewBuilder
+    var customInspectableContent: some View {
+        let indexedLabels = Array(labels.enumerated())
+        ForEach(indexedLabels, id: \.0) { _, label in
+            Text(label)
+        }
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        // This could be a UICollectionView, a UIStackView, or a custom view with some hosted
+        // SwiftUI views (via `UIHostingController`)
+        UIView()
+    }
+
+    func updateUIView(_ view: UIView, context: Context) { }
+}
