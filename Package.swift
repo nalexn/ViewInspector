095cb6633d5210e2cfd1d691b8e302c4e48234b8
Alexey Naumov <anaumov91@gmail.com>
2024-01-18 13:03:09 +0600

    #260: Declare support for visionOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "ViewInspector",
     defaultLocalization: "en",
     platforms: [
-        .macOS(.v10_15), .iOS(.v11), .tvOS(.v13), .watchOS(.v7)
+        .macOS(.v10_15), .iOS(.v12), .tvOS(.v13), .watchOS(.v7), .visionOS(.v1)
     ],
     products: [
         .library(
